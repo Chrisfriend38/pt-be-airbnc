122af4105b2018a919e58146403bc3740ebb4b65
Christopher Friend <christopherfriend@Christophers-iMac.local>
2025-10-29 21:46:32 +0000

    Supabase connection

--- db/connection.js
+++ db/connection.js
@@ -1,13 +1,28 @@
 const { Pool } = require("pg");
 
-require("dotenv").config();
+const ENV = process.env.NODE_ENV || "development";
 
-const pool = new Pool({
+require("dotenv").config({ path: `${__dirname}/../.env.${ENV}`});
+
+const config = {};
+
+if (ENV === "production") {
+    config.connectionString = process.env.DATABASE_URL;
+    config.max = 2;
+}
+
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error("PGDATABASE or DATABASE_URL not set");
+}
+
+/* const pool = new Pool({
     user: process.env.CHRISUSER,
     password: process.env.CHRISPASSWORD,
     host: process.env.CHRISHOST,
     port: process.env.CHRISPORT,
     database: process.env.CHRISDATABASE,
-});
+}); */ 
+
+const pool = new Pool(config);
 
 module.exports = pool;
--- db/seed-run.js
+++ db/seed-run.js
@@ -2,7 +2,6 @@ const db = require("./connection");
 const seed = require("./seed/seed");
 const { usersData, propertyTypesData, propertiesData, reviewsData, imagesData, favouritesData } = require("./data/test");
 
-// Run the seed for all types
 seed(usersData, propertyTypesData, propertiesData, reviewsData, imagesData, favouritesData)
   .then(() => {
     db.end(); 
