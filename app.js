111eed35f52706df7520c60502914a628078d3a8
Chris F <chris@Christophers-MacBook-Pro.local>
2025-11-17 15:46:38 +0000

    Add root router

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 const express = require("express");
-const { getProperties, getPropertyById, getPropertiesByType } = require("./server/controllers/properties");
-const { postReviewsForProperty, deleteReviewsForProperty, getReviewsForProperty } = require("./server/controllers/reviews");
+const {
+  getProperties,
+  getPropertyById,
+  getPropertiesByType,
+} = require("./server/controllers/properties");
+const {
+  postReviewsForProperty,
+  deleteReviewsForProperty,
+  getReviewsForProperty,
+} = require("./server/controllers/reviews");
 const { getUserById } = require("./server/controllers/users");
-const { handlePathNotFound, handleServerErrors, handleCustomErrors } = require("./errors");
-
+const {
+  handlePathNotFound,
+  handleServerErrors,
+  handleCustomErrors,
+} = require("./errors");
 
 const app = express();
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send({ msg: "Welcome to the AirBNC API!" });
+});
 
 app.get("/api/properties", getProperties);
 app.get("/api/properties/:id", getPropertyById);
@@ -15,8 +29,6 @@ app.get("/api/properties/:id/reviews", getReviewsForProperty);
 app.post("/api/properties/:id/reviews", postReviewsForProperty);
 app.delete("/api/properties/:id/reviews", deleteReviewsForProperty);
 app.get("/api/users/:id", getUserById);
-
-
 
 app.use(handlePathNotFound);
 app.use(handleCustomErrors);
